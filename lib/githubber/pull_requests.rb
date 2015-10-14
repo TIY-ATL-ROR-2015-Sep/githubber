9a215134e8484b1c5b125054bb3da0212472d204
Brit Butler <redline6561@gmail.com>
2015-10-14 12:14:47 -0400

    Let's talk about nested hashes!

--- lib/githubber/pull_requests.rb
+++ lib/githubber/pull_requests.rb
@@ -6,6 +6,7 @@ module Githubber
     def initialize(auth_token)
       @auth = {
         "Authorization" => "token #{auth_token}",
+        "Accept"        => "application/vnd.github.v3.text-match+json",
         "User-Agent"    => "HTTParty"
       }
     end
@@ -17,6 +18,14 @@ module Githubber
     def update_user(options={})
       PullRequests.patch("/user", :headers => @auth, :body => options.to_json)
     end
+
+    def code_search(query)
+      response = PullRequests.get("/search/code", headers: @auth, query: { q: query } )
+      response["items"].each do |item|
+        item["text_matches"].each do |match|
+          puts "Found a match! ... " + match["fragment"]
+        end
+      end
+    end
   end
 end
-
